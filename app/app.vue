c242fd4b8a69bf5bda9550172f8ca8cd14fc0e81
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-11-30 14:12:22 +0800

    feat: performance optimization & change background

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,10 @@
 <template>
-	<div :class="{ 'crt-filter': crtShadow }">
-		<background />
-		<div class="relative z-20">
-			<app-header />
-			<div class="mx-auto mt-14 w-4xl max-sm:w-full max-sm:px-4">
-				<NuxtLayout>
-					<NuxtPage />
-				</NuxtLayout>
-			</div>
+	<div :class="{ crt: crtShadow }">
+		<app-header />
+		<div class="mx-auto mt-14 w-4xl max-sm:w-full max-sm:px-4">
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
 		</div>
 	</div>
 </template>
